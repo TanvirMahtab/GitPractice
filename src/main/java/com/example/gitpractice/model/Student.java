66eec6ad6b37a85ee36f4b68510dcb06d7b9d486
tanvir_mahtab <www.tanvirahmed97@gmail.com>
2021-12-20 00:34:09 +0600

    Student class add in model package

---
+++ src/main/java/com/example/gitpractice/model/Student.java
@@ -0,0 +1,72 @@
+package com.example.gitpractice.model;
+
+public class Student {
+    private int id;
+    private String name;
+    private String gender;
+    private String email;
+    private String dateOfBirth;
+    private String mobileNumber;
+
+    public Student(int id,
+                   String name,
+                   String gender,
+                   String email,
+                   String dateOfBirth,
+                   String mobileNumber) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.email = email;
+        this.dateOfBirth = dateOfBirth;
+        this.mobileNumber = mobileNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+}
